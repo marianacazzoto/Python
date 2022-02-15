508d3fd397a871aa6b9bb41d2b5caa5e9765e780
marianacazzoto <MARIANA.CAZZOTO@bandtec.com.br>
2022-02-15 11:30:44 -0300

    Curso de Python

--- Curso de Python/Aula01/app.py
+++ Curso de Python/Aula01/app.py
@@ -1,11 +1,1 @@
-nome = "Mariana"
-idade = "19";
-gosto_de = "Séries"
-
-print(f"Meu nome é {nome}")
-print(f"Eu gosto de {gosto_de}")
-print(f"Eu tenho {idade} anos")
-
-idade = "20"
-
-print(f"{nome} gosta {gosto_de} e tem {idade} anos.")
+print("Hello World")
---
+++ Curso de Python/Aula03/app.py
@@ -0,0 +1,30 @@
+import math
+
+num1 = 5
+num2 = 3.5
+
+print(type(num1))
+print(type(num2))
+
+a = float(5) #conversão de int para float
+print(a)
+
+#type retorna qual o tipo de dado estamos utilizando
+
+num1 + num2
+num1 - num2
+num1 * num2
+num1 / num2
+num1 ** num2 #exponenciação
+num1 // num2 #arredonda 
+num1 % num2 #resto da divisão
+
+abs(-15) #Retorna o valor absoluto
+pow(3,3) #faz a exponenciação
+max(1,5) #retorna o valor maximo
+min(1,5) #retorna o vaor minimo
+round(8.3) #Arredonda para o mais proximo
+
+math.floor(8.8) #Arredondamento pra baixo
+math.ceil(8.8) #Arredondamento pra cima
+math.sqrt(9) #Retorna a raiz quadrada
