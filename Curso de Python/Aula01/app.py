30c466e82e2f2cf3ac32f842be38a31ff7f3b67d
marianacazzoto <MARIANA.CAZZOTO@bandtec.com.br>
2022-02-15 10:43:15 -0300

    Curso de Python

---
+++ Curso de Python/Aula01/app.py
@@ -0,0 +1,11 @@
+nome = "Mariana"
+idade = "19";
+gosto_de = "Séries"
+
+print(f"Meu nome é {nome}")
+print(f"Eu gosto de {gosto_de}")
+print(f"Eu tenho {idade} anos")
+
+idade = "20"
+
+print(f"{nome} gosta {gosto_de} e tem {idade} anos.")
